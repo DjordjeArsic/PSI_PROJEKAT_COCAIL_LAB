7f99142a68a6be7873fc90a0b0b433f2267825e5
Jelena Dragojević <jp.dragojevic@gmail.com>
2020-05-13 21:58:45 +0200

    faza 6 - login i registracija

---
+++ Faza 6/Marko i Jelena/projekatPSI/app/Models/RegistrovaniModel.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RegistrovaniModel extends Model
+{
+        protected $table      = 'registrovani';
+        protected $primaryKey = 'idRegistrovanog';
+        protected $returnType = 'object';
+    
+        protected $allowedFields = [ 'idRegistrovanog', 'obrisanNalog'];
+        
+        //dodati f-ju koja menja obrisanNalog na 1
+}
